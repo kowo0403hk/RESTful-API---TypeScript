488ac835bfc9e96632eeb2922875fc487b5ac89f
Alex Cheung <alexc.sfu@gmail.com>
2022-07-28 02:15:44 -0700

    added validation middleware for registering new user, and the schema for validation

---
+++ src/middleware/validateRequest.ts
@@ -0,0 +1,23 @@
+import { AnySchema } from "yup";
+import { Request, Response, NextFunction } from "express";
+import log from "../logger";
+
+// to validate the post request contains the schema required
+const validate =
+  (schema: AnySchema) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await schema.validate({
+        body: req.body,
+        query: req.query,
+        params: req.params,
+      });
+
+      return next();
+    } catch (e) {
+      log.error(e);
+      return res.status(400).send(e.errors);
+    }
+  };
+
+export default validate;
--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,16 @@
 // when importing with capital letter, it means we are importing the interface
 import { Express, Request, Response } from "express";
+import { createUserHandler } from "./controller/user.controller";
+import validateRequest from "./middleware/validateRequest";
+import { createUserSchema } from "./schema/user.schema";
+
+// basically, all the routes here will go through their respective middlewares to validate request, and then move onto next(), which is the respective hanlder function, and those handler functions are controllers for routing
 
 export default function router(app: Express) {
   app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
 
   // Register user(/api/user)
-  app.post("/api/users");
+  app.post("/api/users", validateRequest(createUserSchema), createUserHandler);
 
   // Login POST (/api/sessions)
 
