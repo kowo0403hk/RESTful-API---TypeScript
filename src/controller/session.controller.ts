9a58ec2c1467ded744539a739e9eea131afa8706
Alex Cheung <alexc.sfu@gmail.com>
2022-07-28 23:21:34 -0700

    added createSessionHandler

--- src/controller/session.controller.ts
+++ src/controller/session.controller.ts
@@ -1,6 +1,8 @@
 import { validatePassword } from "../service/user.service";
 import { Request, Response } from "express";
-import { createSession } from "../services/session.service";
+import { createSession, createAccessToken } from "../service/session.service";
+import { sign } from "../utils/jwt.utils";
+import config from "config";
 
 export async function createUserSessionHandler(req: Request, res: Response) {
   // validate the email and password
@@ -14,6 +16,14 @@ export async function createUserSessionHandler(req: Request, res: Response) {
   const session = await createSession(user._id, req.get("user-agent") || "");
 
   // create access token
+  const accessToken = createAccessToken({
+    user,
+    session,
+  });
+
   // create refresh token
+  const refreshToken = sign(session, {
+    expiresIn: config.get("refreshTokenTtl"), // 1 year
+  });
   // send refresh & access token back
 }
